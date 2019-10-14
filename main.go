9b2c4eb0bd4fe24d04c0abeb86f598c20296bbca
Donatien <donatiennambinintsoa@gmail.com>
2019-10-14 14:28:54 +0300

    Change 1

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package Handler
 
-import "net/http"
 import "github.com/julienschmidt/httprouter"
 import "github.com/donatien513/Contact-Me-Flow/route"
 import "github.com/donatien513/Contact-Me-Flow/utils"
 
-  utils.InitRedisClient()
-  Handler := httprouter.New()
-  Handler.POST("/", route.HeartbeatHandler)
-  Handler.POST("/ask-send", route.AuthentificationHandler)
+utils.InitRedisClient()
+Handler := httprouter.New()
+Handler.POST("/", route.HeartbeatHandler)
+Handler.POST("/ask-send", route.AuthentificationHandler)
