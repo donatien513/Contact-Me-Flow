57e3c5b81c0577ed9c2246405120fc7c3f0c1a77
Donatien <donatiennambinintsoa@gmail.com>
2019-10-14 16:52:39 +0300

    replace heartbeat.go with index.html

--- main.go
+++ main.go
@@ -6,10 +6,6 @@ import "github.com/donatien513/Contact-Me-Flow/utils"
 
 func Handler(w http.ResponseWriter, r *http.Request) {
   utils.InitRedisClient()
-  if r.URL.Path == "/" && r.Method == http.MethodGet {
-    routes.HeartbeatHandler(w, r)
-    return
-  }
   if r.URL.Path == "/ask-send" && r.Method == http.MethodPost {
     routes.AuthentificationHandler(w, r)
     return
--- routes/heartbeat.go
+++
@@ -1,9 +0,0 @@
-package routes
-
-import "net/http"
-
-func HeartbeatHandler(w http.ResponseWriter, r *http.Request) {
-  w.WriteHeader(http.StatusOK)
-  w.Header().Set("Content-Type", "text/plain")
-  w.Write([]byte("I am working :)"))
-}
