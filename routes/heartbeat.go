1da125e29144048609e491047b986f0459f084f1
Donatien <donatiennambinintsoa@gmail.com>
2019-10-14 16:47:38 +0300

    update

--- routes/heartbeat.go
+++ routes/heartbeat.go
@@ -4,10 +4,7 @@ import "net/http"
 import "io/ioutil"
 
 func HeartbeatHandler(w http.ResponseWriter, r *http.Request) {
-  defer r.Body.Close()
   w.WriteHeader(http.StatusOK)
   w.Header().Set("Content-Type", "text/plain")
-  resBody, _ := ioutil.ReadAll(r.Body)
-  print(string(resBody))
-  w.Write(resBody)
+  w.Write([]byte("I am working :)"))
 }
