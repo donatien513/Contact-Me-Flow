25324f06c949204116c66f0e11c9f70811d9e3f1
Donatien <donatiennambinintsoa@gmail.com>
2019-10-14 14:27:04 +0300

    use router directly

--- main.go
+++ main.go
@@ -5,10 +5,7 @@ import "github.com/julienschmidt/httprouter"
 import "github.com/donatien513/Contact-Me-Flow/route"
 import "github.com/donatien513/Contact-Me-Flow/utils"
 
-func Handler() http.HandlerFunc {
   utils.InitRedisClient()
-  router := httprouter.New()
-  router.POST("/", route.HeartbeatHandler)
-  router.POST("/ask-send", route.AuthentificationHandler)
-  return router
-}
+  Handler := httprouter.New()
+  Handler.POST("/", route.HeartbeatHandler)
+  Handler.POST("/ask-send", route.AuthentificationHandler)
